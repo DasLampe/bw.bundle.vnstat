e2a05b3e0bf46ccfbcbf7cae7cec8766d40bd182
Rico Ullmann <rico@erinnerungsfragmente.de>
2016-08-31 15:42:56 +0200

    initial commit

---
+++ items.py
@@ -0,0 +1,62 @@
+pkg_yum = {}
+
+# Make sure you've added the epel-bundle to the nodes if on CentOS/RHEL!
+if node.has_bundle("epel"):
+    pkg_yum['vnstat'] = {
+        'needs': [
+            "pkg_yum:epel-release",
+        ],
+    }
+else:
+    pkg_yum['vnstat'] = {}
+
+svc_systemd = {
+    'vnstat': {
+        'enabled': True,
+        'needs': [
+            "pkg_yum:vnstat",
+        ],
+    },
+}
+
+actions = {}
+
+files = {
+    '/etc/sysconfig/vnstat': {
+        'source': "sysconfig_vnstat",
+        'owner': "root",
+        'mode': "0644",
+        'content_type': "mako",
+        'needs': [
+            "pkg_yum:vnstat",
+        ],
+        'triggers': [
+            "svc_systemd:vnstat:restart",
+        ],
+    },
+    '/etc/vnstat.conf': {
+        'source': "vnstat.conf",
+        'owner': "root",
+        'mode': "0644",
+        'content_type': "mako",
+        'needs': [
+            "pkg_yum:vnstat",
+        ],
+        'triggers': [
+            "svc_systemd:vnstat:restart",
+        ],
+    },
+}
+
+for interface in node.metadata['interfaces']:
+    actions['vnstat_create_database_{}'.format(interface)] = {
+        'command': "vnstat -u -i {}".format(interface),
+        'unless': "test -f /var/lib/vnstat/{}".format(interface),
+        'cascade_skip': False,
+        'needs': [
+            "pkg_yum:vnstat",
+        ],
+        'triggers': [
+            "svc_systemd:vnstat:restart",
+        ],
+    }
