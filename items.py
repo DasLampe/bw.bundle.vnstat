c9a577f8faa6bdab40650fb1ab2d0325699b912b
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 20:06:15 +0200

    proper formatting everywhere

--- items.py
+++ items.py
@@ -5,7 +5,7 @@ pkg_dnf = {
 svc_systemd = {
     'vnstat': {
         'needs': [
-            "pkg_dnf:vnstat",
+            'pkg_dnf:vnstat',
         ],
     },
 }
@@ -14,48 +14,48 @@ actions = {}
 
 files = {
     '/etc/sysconfig/vnstat': {
-        'source': "sysconfig_vnstat",
-        'mode': "0644",
-        'content_type': "mako",
+        'source': 'sysconfig_vnstat',
+        'mode': '0644',
+        'content_type': 'mako',
         'needs': [
-            "pkg_dnf:vnstat",
+            'pkg_dnf:vnstat',
         ],
         'triggers': [
-            "svc_systemd:vnstat:restart",
+            'svc_systemd:vnstat:restart',
         ],
     },
     '/etc/vnstat.conf': {
-        'source': "vnstat.conf",
-        'mode': "0644",
-        'content_type': "mako",
+        'source': 'vnstat.conf',
+        'mode': '0644',
+        'content_type': 'mako',
         'needs': [
-            "pkg_dnf:vnstat",
+            'pkg_dnf:vnstat',
         ],
         'triggers': [
-            "svc_systemd:vnstat:restart",
+            'svc_systemd:vnstat:restart',
         ],
     },
 }
 
 directories = {
-    "/var/lib/vnstat": {
-        "mode": "6755",
-        "owner": "vnstat",
+    '/var/lib/vnstat': {
+        'mode': '6755',
+        'owner': 'vnstat',
         'needs': [
-            "pkg_dnf:vnstat",
+            'pkg_dnf:vnstat',
         ],
     },
 }
 
 for interface in node.metadata['interfaces']:
     actions['vnstat_create_database_{}'.format(interface)] = {
-        'command': "vnstat -u -i {}".format(interface),
-        'unless': "test -f /var/lib/vnstat/{}".format(interface),
+        'command': 'vnstat -u -i {}'.format(interface),
+        'unless': 'test -f /var/lib/vnstat/{}'.format(interface),
         'cascade_skip': False,
         'needs': [
-            "pkg_dnf:vnstat",
+            'pkg_dnf:vnstat',
         ],
         'triggers': [
-            "svc_systemd:vnstat:restart",
+            'svc_systemd:vnstat:restart',
         ],
     }
